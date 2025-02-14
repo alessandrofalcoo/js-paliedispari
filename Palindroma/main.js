a90b57a1660c2b64896de46651280547b6668b5c
alessandrofalcoo <alefalco.1003@gmail.com>
2025-02-14 15:46:45 +0100

    complete palindrome exercise

--- Palindroma/main.js
+++ Palindroma/main.js
@@ -1,11 +1,28 @@
 /* Palindroma
 Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma */
 
+
+// Creo la costante che contiene il valore input dell'utente
 const user_word = prompt("Inserisci una parola e ti dirò se è palindroma");
 
-function checkPalindrom (word){
-    word = 'Osso';
-    const separator = word.split(' ');
-    console.log(separator);
-    
+
+// Creo la funzione per invertire i caratteri di una parola 
+function checkPalindrom(word) {
+
+   
+    return word == word.split('').reverse().join('');
+
 }
+
+// Richiamo la funzione e imposto come argomento la costante precendentemente creata
+checkPalindrom(user_word)
+
+
+// Creo un condizionale per verificare se la parola inserita dall'utente è palindroma o no
+ if (checkPalindrom(user_word)) {
+        console.log('La parola è palindroma');
+
+    } else {
+        console.log('La parola non è palindroma');
+
+    }

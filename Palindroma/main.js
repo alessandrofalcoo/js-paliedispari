0895145734b917907c6a4cf8beb9dfa5741587df
alessandrofalcoo <alefalco.1003@gmail.com>
2025-02-14 15:18:21 +0100

    add function checkPalindrom

--- Palindroma/main.js
+++ Palindroma/main.js
@@ -1,4 +1,11 @@
-/* Palidroma
+/* Palindroma
 Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma */
 
+const user_word = prompt("Inserisci una parola e ti dirò se è palindroma");
 
+function checkPalindrom (word){
+    word = 'Osso';
+    const separator = word.split(' ');
+    console.log(separator);
+    
+}
